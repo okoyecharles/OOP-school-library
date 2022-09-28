36904019223069b8f158bd8d2aa20f82b9240e92
OkoyeCharles <okoyecharles509@gmail.com>
2022-09-28 15:04:07 +0100

    Add reviewer changes

--- ruby/rental.rb
+++ ruby/rental.rb
@@ -1,5 +1,5 @@
 class Rental
-  attr_accessor :date
+  attr_accessor :date, :person, :book
 
   def initialize(date, person, book)
     @date = date
@@ -8,6 +8,6 @@ class Rental
     person.rentals << self
 
     @book = book
-    book.rentals << self
+    book.rentals << self unless book.rentals.include? self
   end
 end
