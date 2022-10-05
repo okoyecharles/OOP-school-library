75bfc2a4d663424e7ba3105da9a04da2081fc975
OkoyeCharles <okoyecharles509@gmail.com>
2022-10-05 13:39:19 +0100

    Add first three tests

--- ruby/person.rb
+++ ruby/person.rb
@@ -18,7 +18,7 @@ class Person < Nameable
   end
 
   def can_use_services?
-    is_of_age? || @parent_permission
+    of_age? || @parent_permission
   end
 
   private
---
+++ spec/spec_helper.rb
@@ -0,0 +1,4 @@
+require_relative '../ruby/book'
+require_relative '../ruby/person'
+require_relative '../ruby/student'
+require_relative '../ruby/classroom'
